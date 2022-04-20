2ceeaf99dd42a48b789c0a2e85a9e45e2f91f984
Mika Vainio <koti.mika.vainio@gmail.com>
2022-04-20 14:13:30 +0300

    Started creating the ui

---
+++ productCatalog.py
@@ -0,0 +1,8 @@
+# AN APPLICATION TO CREATE PRODUCT CATALOG PAGES FOR RASEKO TOOL LENDING SOLUTION
+# -------------------------------------------------------------------------------
+
+# LIBRARIES AND MODULES
+
+
+import productBarcode # Module for making EAN or Code 128 barcodes
+
